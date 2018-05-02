grkvlt/sawtooth-go-sdk

Update local files to point at grkvlt repo

messaging/connection.go
@@ -24,8 +24,8 @@ import (
 	"github.com/golang/protobuf/proto"
 	zmq "github.com/pebbe/zmq4"
 	uuid "github.com/satori/go.uuid"
-	"github.com/rberg2/sawtooth-go-sdk/logging"
-	"github.com/rberg2/sawtooth-go-sdk/protobuf/validator_pb2"
+	"github.com/grkvlt/sawtooth-go-sdk/logging"
+	"github.com/grkvlt/sawtooth-go-sdk/protobuf/validator_pb2"
 )
 
 var logger *logging.Logger = logging.Get()

processor/context.go
@@ -20,11 +20,11 @@ package processor
 import (
 	"fmt"
 	"github.com/golang/protobuf/proto"
-	"github.com/rberg2/sawtooth-go-sdk/messaging"
-	"github.com/rberg2/sawtooth-go-sdk/protobuf/events_pb2"
-	"github.com/rberg2/sawtooth-go-sdk/protobuf/state_context_pb2"
-	"github.com/rberg2/sawtooth-go-sdk/protobuf/client_state_pb2"
-	"github.com/rberg2/sawtooth-go-sdk/protobuf/validator_pb2"
+	"github.com/grkvlt/sawtooth-go-sdk/messaging"
+	"github.com/grkvlt/sawtooth-go-sdk/protobuf/events_pb2"
+	"github.com/grkvlt/sawtooth-go-sdk/protobuf/state_context_pb2"
+	"github.com/grkvlt/sawtooth-go-sdk/protobuf/client_state_pb2"
+	"github.com/grkvlt/sawtooth-go-sdk/protobuf/validator_pb2"
 )
 
 // Context provides an abstract interface for getting and setting validator

processor/handler.go
@@ -27,7 +27,7 @@
 package processor
 
 import (
-	"github.com/rberg2/sawtooth-go-sdk/protobuf/processor_pb2"
+	"github.com/grkvlt/sawtooth-go-sdk/protobuf/processor_pb2"
 )
 
 // TransactionHandler is the interface that defines the business logic for a

processor/processor.go
@@ -24,11 +24,11 @@ import (
 	"os"
 	"os/signal"
 	"runtime"
-	"github.com/rberg2/sawtooth-go-sdk/logging"
-	"github.com/rberg2/sawtooth-go-sdk/messaging"
-	"github.com/rberg2/sawtooth-go-sdk/protobuf/network_pb2"
-	"github.com/rberg2/sawtooth-go-sdk/protobuf/processor_pb2"
-	"github.com/rberg2/sawtooth-go-sdk/protobuf/validator_pb2"
+	"github.com/grkvlt/sawtooth-go-sdk/logging"
+	"github.com/grkvlt/sawtooth-go-sdk/messaging"
+	"github.com/grkvlt/sawtooth-go-sdk/protobuf/network_pb2"
+	"github.com/grkvlt/sawtooth-go-sdk/protobuf/processor_pb2"
+	"github.com/grkvlt/sawtooth-go-sdk/protobuf/validator_pb2"
 )
 
 var logger *logging.Logger = logging.Get()

processor/worker.go
@@ -21,10 +21,10 @@ import (
 	"fmt"
 	"github.com/golang/protobuf/proto"
 	zmq "github.com/pebbe/zmq4"
-	"github.com/rberg2/sawtooth-go-sdk/messaging"
-	"github.com/rberg2/sawtooth-go-sdk/protobuf/processor_pb2"
-	"github.com/rberg2/sawtooth-go-sdk/protobuf/transaction_pb2"
-	"github.com/rberg2/sawtooth-go-sdk/protobuf/validator_pb2"
+	"github.com/grkvlt/sawtooth-go-sdk/messaging"
+	"github.com/grkvlt/sawtooth-go-sdk/protobuf/processor_pb2"
+	"github.com/grkvlt/sawtooth-go-sdk/protobuf/transaction_pb2"
+	"github.com/grkvlt/sawtooth-go-sdk/protobuf/validator_pb2"
 )
 
 // The main worker thread finds an appropriate handler and processes the request

trecemgroup/sawtooth-sdk-go

messaging/connection.go
@@ -22,10 +22,10 @@ package messaging
 import (
 	"fmt"
 	"github.com/golang/protobuf/proto"
-	"github.com/hyperledger/sawtooth-sdk-go/logging"
-	"github.com/hyperledger/sawtooth-sdk-go/protobuf/validator_pb2"
 	zmq "github.com/pebbe/zmq4"
 	uuid "github.com/satori/go.uuid"
+	"github.com/grkvlt/sawtooth-sdk-go/logging"
+	"github.com/grkvlt/sawtooth-sdk-go/protobuf/validator_pb2"
 )
 
 var logger *logging.Logger = logging.Get()

processor/context.go
@@ -20,10 +20,11 @@ package processor
 import (
 	"fmt"
 	"github.com/golang/protobuf/proto"
-	"github.com/hyperledger/sawtooth-sdk-go/messaging"
-	"github.com/hyperledger/sawtooth-sdk-go/protobuf/events_pb2"
-	"github.com/hyperledger/sawtooth-sdk-go/protobuf/state_context_pb2"
-	"github.com/hyperledger/sawtooth-sdk-go/protobuf/validator_pb2"
+	"github.com/grkvlt/sawtooth-sdk-go/messaging"
+	"github.com/grkvlt/sawtooth-sdk-go/protobuf/events_pb2"
+	"github.com/grkvlt/sawtooth-sdk-go/protobuf/state_context_pb2"
+	"github.com/grkvlt/sawtooth-sdk-go/protobuf/client_state_pb2"
+	"github.com/grkvlt/sawtooth-sdk-go/protobuf/validator_pb2"
 )
 
 // Context provides an abstract interface for getting and setting validator

processor/handler.go
@@ -27,7 +27,7 @@
 package processor
 
 import (
-	"github.com/hyperledger/sawtooth-sdk-go/protobuf/processor_pb2"
+	"github.com/grkvlt/sawtooth-sdk-go/protobuf/processor_pb2"
 )
 
 // TransactionHandler is the interface that defines the business logic for a

processor/processor.go
@@ -20,15 +20,15 @@ package processor
 import (
 	"fmt"
 	"github.com/golang/protobuf/proto"
-	"github.com/hyperledger/sawtooth-sdk-go/logging"
-	"github.com/hyperledger/sawtooth-sdk-go/messaging"
-	"github.com/hyperledger/sawtooth-sdk-go/protobuf/network_pb2"
-	"github.com/hyperledger/sawtooth-sdk-go/protobuf/processor_pb2"
-	"github.com/hyperledger/sawtooth-sdk-go/protobuf/validator_pb2"
 	zmq "github.com/pebbe/zmq4"
 	"os"
 	"os/signal"
 	"runtime"
+	"github.com/grkvlt/sawtooth-sdk-go/logging"
+	"github.com/grkvlt/sawtooth-sdk-go/messaging"
+	"github.com/grkvlt/sawtooth-sdk-go/protobuf/network_pb2"
+	"github.com/grkvlt/sawtooth-sdk-go/protobuf/processor_pb2"
+	"github.com/grkvlt/sawtooth-sdk-go/protobuf/validator_pb2"
 	"time"
 )
 

processor/worker.go
@@ -20,11 +20,11 @@ package processor
 import (
 	"fmt"
 	"github.com/golang/protobuf/proto"
-	"github.com/hyperledger/sawtooth-sdk-go/messaging"
-	"github.com/hyperledger/sawtooth-sdk-go/protobuf/processor_pb2"
-	"github.com/hyperledger/sawtooth-sdk-go/protobuf/transaction_pb2"
-	"github.com/hyperledger/sawtooth-sdk-go/protobuf/validator_pb2"
 	zmq "github.com/pebbe/zmq4"
+	"github.com/grkvlt/sawtooth-sdk-go/messaging"
+	"github.com/grkvlt/sawtooth-sdk-go/protobuf/processor_pb2"
+	"github.com/grkvlt/sawtooth-sdk-go/protobuf/transaction_pb2"
+	"github.com/grkvlt/sawtooth-sdk-go/protobuf/validator_pb2"
 )
 
 // The main worker thread finds an appropriate handler and processes the request